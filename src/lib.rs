06adcf0af15c9d6db05010b996fabdc0178f58da
郑友捷 <Azure_stars@126.com>
2024-05-01 18:00:58 +0800

    fix: remove axlibc from dependency

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,5 @@ mod file;
 pub use file::fs_init;
 mod api;
 pub use api::{println, run_testcase};
-#[cfg(feature = "ext4fs")]
-#[allow(unused_imports)]
-use axlibc::ax_open;
 
 pub use linux_syscall_api::{init_current_dir, recycle_user_process};
